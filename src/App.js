7e067dbfe1a137cf19b5e59dabd53bd3c9338137
farukwebcode21 <farukahmadmk@yahoo.com>
2023-03-16 22:42:04 +0600

    Create account

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./App.css";
+import app from "./firebase/firebase.init";
+
+const auth = getAuth(app);
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const handleGooglePassword = (e) => {
+    e.preventDefault();
+    setSuccess(false);
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    if (
+      !/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password)
+    ) {
+      setPassword(
+        "Min 8 Letter al least a symobol & upper and lowercase letter and number"
+      );
+      return;
+    }
+    setPassword("");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+        setPassword(error.message);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-50 mx-auto">
+      <h2>Email and Password Registation </h2>
+      <Form onSubmit={handleGooglePassword}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            required
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <p className="text-danger">{password}</p>
+        {success && <p className="text-success">User Create Success</p>}
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
     </div>
   );
 }
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANf4GyapRaULPiOcyN64524ho8-V54FH0",
+  authDomain: "googl-email-password-login.firebaseapp.com",
+  projectId: "googl-email-password-login",
+  storageBucket: "googl-email-password-login.appspot.com",
+  messagingSenderId: "750397786738",
+  appId: "1:750397786738:web:8b95cc235e21b3012550eb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
